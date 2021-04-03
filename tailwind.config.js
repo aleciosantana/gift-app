0b69d3243f473d2d51d15d3c603f9ec5d1b60b1e
Alécio Santana <aleciosantana@gmail.com>
2021-04-03 20:09:47 -0300

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        128: '32rem'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
