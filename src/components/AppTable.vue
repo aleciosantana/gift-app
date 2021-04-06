95469f26d733e69cc03205f240b9bdf638139f4d
Alécio Santana <aleciosantana@gmail.com>
2021-04-06 18:50:14 -0300

    Lists the products on the dashboard

---
+++ src/components/AppTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <table class="app-table">
+    <slot></slot>
+  </table>
+</template>
+
+<style lang="postcss">
+table.app-table {
+  @apply border-collapse bg-white rounded-md shadow-lg dark:bg-gray-800 w-full
+}
+
+.app-table thead tr {
+  @apply font-bold text-white bg-gray-800 dark:bg-gray-700 border-b-8 border-white dark:border-gray-800
+}
+
+.app-table thead th {
+  @apply px-4 py-2
+}
+
+.app-table thead th:first-child {
+  @apply rounded-tl-md
+}
+.app-table thead th:last-child {
+  @apply rounded-tr-md
+}
+
+.app-table tbody tr {
+  @apply border-t hover:bg-gray-100 dark:hover:bg-gray-700 dark:border-gray-700  text-sm text-gray-600 dark:text-white
+}
+
+.app-table tbody td {
+  @apply px-4 py-3
+}
+
+.app-table tbody tr:last-child td:first-child {
+  @apply rounded-bl-md
+}
+
+.app-table tbody tr:last-child td:last-child {
+  @apply rounded-br-md
+}
+</style>
--- src/views/dashboard-view/DashboardView.vue
+++ src/views/dashboard-view/DashboardView.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Dashboard</h1>
+  <section class="container p-6 mx-auto">
+    <h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl">Ofertas</h2>
+
+    <div class="mt-4">
+      <app-table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <td>{{ product.title }}</td>
+            <td class="text-right">$ {{ product.value }}</td>
+          </tr>
+        </tbody>
+      </app-table>
+    </div>
+  </section>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppTable from '@/components/AppTable.vue'
+import { useProductMixin } from '@/mixins'
+
+export default defineComponent({
+  components: { AppTable },
+  setup () {
+    const { products, fetchProducts } = useProductMixin()
+
+    fetchProducts()
+
+    return { products }
+  }
+})
+</script>
