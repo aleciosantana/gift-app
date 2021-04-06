75b1a6f4ccdff0d85c5bc1cd6365f7cab9d2238a
Alécio Santana <aleciosantana@gmail.com>
2021-04-06 18:32:37 -0300

    List the products on the home page

--- src/views/home-view/HomeView.vue
+++ src/views/home-view/HomeView.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Home</h1>
+  <section class="container p-6 mx-auto">
+    <h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl">Products</h2>
+
+    <div class="flex">
+      <div class="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <home-product-card
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        ></home-product-card>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HomeProductCard from './components/HomeProductCard.vue'
+import { useProductMixin } from '@/mixins'
+
+export default defineComponent({
+  components: { HomeProductCard },
+  setup () {
+    const { products, fetchProducts } = useProductMixin()
+
+    fetchProducts()
+
+    return { products }
+  }
+})
+</script>
---
+++ src/views/home-view/components/HomeProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+    <div class="px-4 py-2 h-32 overflow-hidden">
+      <h1 class="text-3xl font-bold text-gray-800 uppercase dark:text-white truncate">{{ product.title }}</h1>
+      <p class="mt-1 text-sm text-gray-600 overflow-ellipsis overflow-hidden dark:text-gray-400">{{ product.description }}</p>
+    </div>
+
+    <img class="object-cover w-full h-48 mt-2" :src="product.image">
+
+    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+      <h1 class="text-lg font-bold text-white">${{ product.value }}</h1>
+      <button class="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-200 bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Adicionar</button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Product } from '@/types'
+
+export default defineComponent({
+  props: {
+    product: Object as PropType<Product>
+  }
+})
+</script>
